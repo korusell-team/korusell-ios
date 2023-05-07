a8fc984161c1c7a0f7b4d71a73f9523f7437c8b2
Sergey Lee <sergey-li@thewavetalk.com>
2023-05-07 09:22:33 +0900

    Initial Commit

---
+++ korusell/korusellApp.swift
@@ -0,0 +1,17 @@
+//
+//  korusellApp.swift
+//  korusell
+//
+//  Created by Sergey Lee on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct korusellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
