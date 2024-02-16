1e446d97028ccea9d4095fedf813c4cb839d0b82
sergey-lee <guagetru.bla@gmail.com>
2024-02-16 10:29:44 +0900

    contacts / search: focus on appear

--- korusell/UI/Session/Search/SearchBar.swift
+++ korusell/UI/Session/Search/SearchBar.swift
@@ -9,63 +9,65 @@ import SwiftUI
 
 struct SearchBar: View {
     @EnvironmentObject var cc: ContactsController
-//    @Binding var searching: Bool
-//    @Binding var searchField: String
+    //    @Binding var searching: Bool
+    //    @Binding var searchField: String
     @Binding var searchPresented: Bool
     @FocusState var isEditing: Bool
-//    var isEditing: FocusState<Bool>.Binding
+    //    var isEditing: FocusState<Bool>.Binding
     
     var body: some View {
         HStack {
             TextField("Поиск", text: $cc.searchField, onCommit: {
                 // TODO: Test it out!
-//                cc.selectedCategory = listOfCategories.filter { $0.name.lowercased() == cc.selectedSubcategory.bound.lowercased() }.first
-                })
+                //                cc.selectedCategory = listOfCategories.filter { $0.name.lowercased() == cc.selectedSubcategory.bound.lowercased() }.first
+            })
             .autocorrectionDisabled()
             .textInputAutocapitalization(.never)
-                .padding(7)
-                .padding(.horizontal, 25)
-                .background(Color.gray50)
-                .cornerRadius(8)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width, alignment: .leading)
-                            .padding(.leading, 8)
-                 
-//                        if cc.searching && !cc.searchField.isEmpty {
-//                            Button(action: {
-//                                cc.searchField = ""
-//                            }) {
-//                                Image(systemName: "multiply.circle.fill")
-//                                    .foregroundColor(.gray)
-//                                    .padding(.trailing, 8)
-//                            }
-//                        }
-                    }
-                )
-                .focused($isEditing)
-            
-//            if $isEditing.wrappedValue {
-                Button(action: {
-                    withAnimation {
-                        searchPresented = false
-                        cc.searchField = ""
-                        cc.searching = false
-                        cc.resetCategories()
-                        $isEditing.wrappedValue = false
-                    }
-                }) {
-                    Text("Отмена")
+            .padding(7)
+            .padding(.horizontal, 25)
+            .background(Color.gray50)
+            .cornerRadius(8)
+            .overlay(
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                        .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width, alignment: .leading)
+                        .padding(.leading, 8)
+                    
+                    //                        if cc.searching && !cc.searchField.isEmpty {
+                    //                            Button(action: {
+                    //                                cc.searchField = ""
+                    //                            }) {
+                    //                                Image(systemName: "multiply.circle.fill")
+                    //                                    .foregroundColor(.gray)
+                    //                                    .padding(.trailing, 8)
+                    //                            }
+                    //                        }
                 }
-//                .padding(.trailing, 10)
-                .transition(.scale)
-//            }
+            )
+            .focused($isEditing)
+            
+            //            if $isEditing.wrappedValue {
+            Button(action: {
+                withAnimation {
+                    searchPresented = false
+                    cc.searchField = ""
+                    cc.searching = false
+                    cc.resetCategories()
+                    $isEditing.wrappedValue = false
+                }
+            }) {
+                Text("Отмена")
+            }
+            .transition(.scale)
+            //            }
         }
         .padding(.horizontal, 20)
         .background(Color.bg)
         .animation(.default, value: isEditing)
+        .onAppear {
+            isEditing = true
+        }
         .onChange(of: isEditing) { editing in
             withAnimation {
                 cc.searching = editing
@@ -76,10 +78,10 @@ struct SearchBar: View {
 
 //struct SearchBar_Previews: PreviewProvider {
 //    static let cc = ContactsController()
-//    
+//
 //    @FocusState var focusedField: Bool?
 //    @State var searching = false
-//    
+//
 //    static var previews: some View {
 //        SearchBar(searching: $searching, isEditing: focusedField)
 //            .environmentObject(cc)
